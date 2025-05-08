acf31d47c2feb1fd4ca5aa2c3f286ad8970c793e
Zaminur151 <hasanzminur@gmail.com>
2025-05-08 21:39:18 +0600

    Carousel impl on onboarding

--- lib/feature/view/onboarding_page.dart
+++ lib/feature/view/onboarding_page.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:time_plan/app/view/theme/theme_cubit.dart';
 import 'package:time_plan/app/view/widget/social_button.dart';
 
@@ -11,8 +13,8 @@ class OnboardingPage extends StatelessWidget {
       body: _Backgroung(
         child: Column(
           children: [
-            Expanded(child: Text("dfhj")),
-            _SocialButtons()
+             Expanded(child: __CarouselWidget()),
+            _SocialButtons(onGoogleLogin: () {  }, onAppleLogin: () {  }, onMailLogin: () {  },)
           ]
         ),
       ),
@@ -45,26 +47,14 @@ class _Backgroung extends StatelessWidget {
 }
 
 class _SocialButtons extends StatelessWidget {
-  const _SocialButtons({super.key});
+  const _SocialButtons({required this.onGoogleLogin, required this.onAppleLogin, required this.onMailLogin});
+
+  final VoidCallback onGoogleLogin;
+  final VoidCallback onAppleLogin;
+  final VoidCallback onMailLogin;
 
   @override
   Widget build(BuildContext context) {
-    // return SizedBox(
-    //   width: double.infinity,
-    //   child: DecoratedBox(
-    //     //width: double.infinity,
-    //     decoration: BoxDecoration(
-    //       borderRadius: BorderRadius.only(topLeft: Radius.circular(16), topRight: Radius.circular(16))
-    //     ),
-    //     child: Column(
-    //       children: [
-    //         SocialButton.google(onTap: (){}),
-    //         SocialButton.apple(onTap: (){}),
-    //         SocialButton.mail(onTap: (){}),
-    //       ],
-    //     ),
-    //   ),
-    // );
     return Container(
       width: double.infinity,
       decoration: BoxDecoration(
@@ -81,10 +71,61 @@ class _SocialButtons extends StatelessWidget {
             SizedBox(width: double.infinity, child: SocialButton.apple(onTap: (){})),
             SizedBox(width: double.infinity, child: SocialButton.mail(onTap: (){})),
             SizedBox(height: 5,)
-            
           ],
         ),
       ),
     );
+  }
+}
+
+class CarouselItem {
+  const CarouselItem({required this.imagePath,  required this.title, required this.subTitle,});
+
+  final String imagePath;
+  final String title;
+  final String subTitle;
+}
+
+final _carouselItems = <CarouselItem>[
+  const CarouselItem(imagePath: "assets/svg/onboarding/carousel3.svg", title: "Get More with Premium\nStart Your Journy", subTitle: "You're an owner of a company or employe you can check your office work time report"),
+   const CarouselItem(imagePath: "assets/svg/onboarding/carousel2.svg", title: "Check Your Work Attendance\nReport on the Go", subTitle: "You're an owner of a company or employe you can check your office work time report"),
+  const CarouselItem(imagePath: "assets/svg/onboarding/carousel1.svg", title: "Get More with Premium\nStart Your Journy", subTitle: "You're an owner of a company or employe you can check your office work time report"),
+];
+
+
+class __CarouselWidget extends StatefulWidget {
+  const __CarouselWidget({super.key});
+
+  @override
+  State<__CarouselWidget> createState() => ___CarouselWidgetState();
+}
+
+class ___CarouselWidgetState extends State<__CarouselWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider.builder(
+      itemCount: _carouselItems.length, 
+      options: CarouselOptions(
+        aspectRatio: 1/1,
+        viewportFraction: 1,
+        autoPlay: true,
+        //enlargeCenterPage: true
+      ),
+      itemBuilder: (context, index, realIndex){
+        final item = _carouselItems[index];
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              SvgPicture.asset(item.imagePath,height: 200,width: 200,),
+              SizedBox(height: 20,),
+              Text(item.title,style: context.textTheme.titleLarge,textAlign: TextAlign.center,),
+              SizedBox(height: 8,),
+              Text(item.subTitle,style: context.textTheme.bodyMedium,textAlign: TextAlign.center,)
+            ],
+          ),
+        );
+      }, 
+      );
   }
 }
